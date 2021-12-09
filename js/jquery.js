86f27e2e6ffc4ab9048f6f0c14675dac89fbde53
Sagnik Saha <sagniksaha20013@gmail.com>
2021-12-09 19:06:58 +0530

    all set for mobile design

--- js/jquery.js
+++ js/jquery.js
@@ -1,26 +1,43 @@
+const SLIDE_LEFT = "animate__animated animate__slideInLeft";
+const FADE_IN = "animate__animated animate__fadeIn";
+const FADE_OUT = "animate__animated animate__fadeOut"
 $(window).on('load',()=>{
     $('#loading').hide();
 })
 
 $(window).on('scroll',()=>{
-    console.log($(window).scrollTop())
-    if($(window).scrollTop()>500){
-        $('#projects').addClass('animate__animated animate__slideInLeft')
-        $('.card-animated').addClass('animate__animated animate__slideInLeft')
+    const scrollTop = $(window).scrollTop();
+    if(scrollTop>500){
+        $('#projects').addClass(SLIDE_LEFT)
+        $('.card-animated').addClass(SLIDE_LEFT)
     }
-    if($(window).scrollTop()<400){
-        $('#projects').removeClass('animate__animated animate__slideInLeft')
-        $('.card').removeClass('animate__animated animate__slideInLeft')
+    if(scrollTop<400){
+        $('#projects').removeClass(SLIDE_LEFT)
+        $('.card-animated').removeClass(SLIDE_LEFT)
     }
-    if($(window).scrollTop()>3988){
-        $('.reason-card-container').removeClass('animate__animated animate__fadeOut')
-        $('.reason-card-container').addClass('animate__animated animate__fadeIn')
+    if(scrollTop>3988){
+        $('.reason-card-container').removeClass(FADE_OUT)
+        $('.reason-card-container').addClass(FADE_IN)
     }
-    if($(window).scrollTop()<3900){
-        $('.reason-card-container').removeClass('animate__animated animate__fadeIn')
-        $('.reason-card-container').addClass('animate__animated animate__fadeOut')
+    if(scrollTop<3890){
+        $('.reason-card-container').removeClass(FADE_IN)
+        $('.reason-card-container').addClass(FADE_OUT)
+    }
+    if(scrollTop>1700){
+        $('#skills').addClass(SLIDE_LEFT)
+        $('.card-animated-skill').addClass(SLIDE_LEFT)
+    }
+    if(scrollTop<1600){
+        $('#skills').removeClass(SLIDE_LEFT)
+        $('.card-animated-skill').removeClass(SLIDE_LEFT)
+    }
+    if(scrollTop>3517){
+        $('#reasons').addClass(SLIDE_LEFT)
+    }
+    if(scrollTop<3510){
+        $('#reasons').removeClass(SLIDE_LEFT)
     }
 })
-$('.nav-link').on('click',()=>{
+$('#navbar-menu ul li a').on('click',()=>{
     $('#navbar-menu').fadeOut(1000)
 })
