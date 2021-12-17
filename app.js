742873b70ca526ff91a50bfd04af71dd15c7db01
Sagnik Saha <sagniksaha20013@gmail.com>
2021-12-17 10:01:40 +0530

    Updated the workflow with background and added shaking to button

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-function toggleNavbar(){const a=document.getElementById("navbar-menu").classList;a.contains("navbar-in")?(a.remove("navbar-in"),a.add("navbar-out")):(document.getElementById("navbar-menu").style.display="block",a.remove("navbar-out"),a.add("navbar-in"))}window.addEventListener("resize",()=>{if(961<window.innerWidth){const a=document.getElementById("navbar-menu");a.style.display="block",a.classList.remove("navbar-out")}});
+function toggleNavbar() {
+  const a = document.getElementById("navbar-menu").classList;
+  a.contains("navbar-in")
+    ? (a.remove("navbar-in"), a.add("navbar-out"))
+    : ((document.getElementById("navbar-menu").style.display = "block"),
+      a.remove("navbar-out"),
+      a.add("navbar-in"));
+}
+window.addEventListener("resize", () => {
+  if (961 < window.innerWidth) {
+    const a = document.getElementById("navbar-menu");
+    (a.style.display = "block"), a.classList.remove("navbar-out");
+  }
+});
--- jquery.js
+++ jquery.js
@@ -1,1 +1,62 @@
-const SLIDE_LEFT="animate__animated animate__slideInLeft",FADE_IN="animate__animated animate__fadeIn",FADE_OUT="animate__animated animate__fadeOut";function removeActive(){const a=document.getElementsByClassName("navbar-links");for(let b=0;b<a.length;b++)a[b].classList.remove("active")}function setActive(a){removeActive();let b="";b=500>a?"about-link":1700>a?"project-link":3517>a?"skill-link":"reason-link",document.getElementById(b).classList.add("active")}$(window).on("load",()=>{$("#loading").hide()}),$(window).on("scroll",()=>{const a=$(window).scrollTop();500<a&&(setActive(a),$("#projects").addClass(SLIDE_LEFT),$(".card-animated").addClass(SLIDE_LEFT)),400>a&&(setActive(a),$("#projects").removeClass(SLIDE_LEFT),$(".card-animated").removeClass(SLIDE_LEFT)),3900<a&&($(".workflow-container").removeClass(FADE_OUT),$(".workflow-container").addClass(FADE_IN)),3890>a&&($(".workflow-container").removeClass(FADE_IN),$(".workflow-container").addClass(FADE_OUT)),5333<a&&($(".reason-card-container").removeClass(FADE_OUT),$(".reason-card-container").addClass(FADE_IN)),3890>a&&($(".reason-card-container").removeClass(FADE_IN),$(".reason-card-container").addClass(FADE_OUT)),1700<a&&(setActive(a),$("#skills").addClass(SLIDE_LEFT),$(".card-animated-skill").addClass(SLIDE_LEFT)),1600>a&&(setActive(a),$("#skills").removeClass(SLIDE_LEFT),$(".card-animated-skill").removeClass(SLIDE_LEFT)),3517<a&&(setActive(a),$("#reasons").addClass(SLIDE_LEFT)),3510>a&&(setActive(a),$("#reasons").removeClass(SLIDE_LEFT))}),$("#navbar-menu ul li a").on("click",()=>{961>window.innerWidth&&$.when($("#navbar-menu").fadeOut(1e3)).then(()=>{document.getElementById("navbar-menu").classList.remove("navbar-in")})});
+const SLIDE_LEFT = "animate__animated animate__slideInLeft",
+  FADE_IN = "animate__animated animate__fadeIn",
+  FADE_OUT = "animate__animated animate__fadeOut";
+function removeActive() {
+  const a = document.getElementsByClassName("navbar-links");
+  for (let b = 0; b < a.length; b++) a[b].classList.remove("active");
+}
+function setActive(a) {
+  removeActive();
+  let b = "";
+  (b =
+    500 > a
+      ? "about-link"
+      : 1700 > a
+      ? "project-link"
+      : 3517 > a
+      ? "skill-link"
+      : "reason-link"),
+    document.getElementById(b).classList.add("active");
+}
+$(window).on("load", () => {
+  $("#loading").hide();
+}),
+  $(window).on("scroll", () => {
+    const a = $(window).scrollTop();
+    500 < a &&
+      (setActive(a),
+      $("#projects").addClass(SLIDE_LEFT),
+      $(".card-animated").addClass(SLIDE_LEFT)),
+      400 > a &&
+        (setActive(a),
+        $("#projects").removeClass(SLIDE_LEFT),
+        $(".card-animated").removeClass(SLIDE_LEFT)),
+      3900 < a &&
+        ($(".workflow-container").removeClass(FADE_OUT),
+        $(".workflow-container").addClass(FADE_IN)),
+      3890 > a &&
+        ($(".workflow-container").removeClass(FADE_IN),
+        $(".workflow-container").addClass(FADE_OUT)),
+      5333 < a &&
+        ($(".reason-card-container").removeClass(FADE_OUT),
+        $(".reason-card-container").addClass(FADE_IN)),
+      3890 > a &&
+        ($(".reason-card-container").removeClass(FADE_IN),
+        $(".reason-card-container").addClass(FADE_OUT)),
+      1700 < a &&
+        (setActive(a),
+        $("#skills").addClass(SLIDE_LEFT),
+        $(".card-animated-skill").addClass(SLIDE_LEFT)),
+      1600 > a &&
+        (setActive(a),
+        $("#skills").removeClass(SLIDE_LEFT),
+        $(".card-animated-skill").removeClass(SLIDE_LEFT)),
+      3517 < a && (setActive(a), $("#reasons").addClass(SLIDE_LEFT)),
+      3510 > a && (setActive(a), $("#reasons").removeClass(SLIDE_LEFT));
+  }),
+  $("#navbar-menu ul li a").on("click", () => {
+    961 > window.innerWidth &&
+      $.when($("#navbar-menu").fadeOut(1e3)).then(() => {
+        document.getElementById("navbar-menu").classList.remove("navbar-in");
+      });
+  });
